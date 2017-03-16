0ed14b13593622184edb0e2f760bed56a8e98ffd
Louremy Pena <louremypena@Louremys-MacBook-Pro.local>
2017-03-15 22:24:26 -0400

    created app files and content for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//Express Configuration
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Using BodyParser NPM makes it possible for our server to interpret data sent to it.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Router: points our server to the app route files
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//Listener: starts our server
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+});
